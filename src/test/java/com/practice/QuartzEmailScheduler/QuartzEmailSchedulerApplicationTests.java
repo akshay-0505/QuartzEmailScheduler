a8c34a8b812968edb307ad140da2f6fdc4de4a05
akshay-0505 <akshaygadhave531@gmail.com>
2025-05-17 17:50:37 +0530

    Intializing Spring Project

---
+++ src/test/java/com/practice/QuartzEmailScheduler/QuartzEmailSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.QuartzEmailScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzEmailSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
